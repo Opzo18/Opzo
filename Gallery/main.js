52abd78d5b0e6fdaab0070ca7b29d03c9e387cc2
Opzo <74737204+Opzo18@users.noreply.github.com>
2023-07-12 12:13:08 +0200

    asd

--- Gallery/main.js
+++ Gallery/main.js
@@ -4,3 +4,26 @@ window.addEventListener("load", function () {
   var loader = document.querySelector(".loader");
   loader.style.display = "none";
 });
+
+
+// Big img
+function loadLargeImage(src) {
+  var largeImage = document.createElement("img");
+  largeImage.src = src;
+  largeImage.style.maxWidth = "70%";
+  largeImage.style.maxHeight = "70%";
+
+  var modal = document.createElement("div");
+  modal.style.position = "fixed";
+  modal.style.top = "0";
+  modal.style.left = "0";
+  modal.style.width = "100%";
+  modal.style.height = "100%";
+  modal.style.backgroundColor = "rgba(0, 0, 0, 0.9)";
+  modal.style.display = "flex";
+  modal.style.justifyContent = "center";
+  modal.style.alignItems = "center";
+
+  modal.appendChild(largeImage);
+  document.body.appendChild(modal);
+}

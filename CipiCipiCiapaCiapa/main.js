e560dd1459f54a275f936ab4e0ddb1b9a56ab947
Opzo <74737204+Opzo18@users.noreply.github.com>
2023-12-16 14:35:37 +0100

    Update main.js

--- CipiCipiCiapaCiapa/main.js
+++ CipiCipiCiapaCiapa/main.js
@@ -1,12 +1,12 @@
 const changeText = [
   { text: "Cipi Cipi", color: "white", time: 1000 },
-  { text: "Chapa Chapa", color: "orange", time: 800 },
+  { text: "Chapa Chapa", color: "red", time: 800 },
   { text: "Dubi Dubi", color: "white", time: 800 },
-  { text: "Daba Daba", color: "orange", time: 800 },
+  { text: "Daba Daba", color: "red", time: 800 },
   { text: "Magico Mi", color: "white", time: 800 },
-  { text: "Dubi Dubi", color: "orange", time: 800 },
+  { text: "Dubi Dubi", color: "red", time: 800 },
   { text: "BOOM", color: "yellow", time: 400 },
-  { text: "BOOM", color: "red", time: 400 },
+  { text: "BOOM", color: "orange", time: 400 },
   { text: "BOOM", color: "lime", time: 400 },
   { text: "BOOM", color: "magenta", time: 300 },
 ];
@@ -18,6 +18,7 @@ function updateText() {
   const currentText = changeText[currentIndex];
   textElement.textContent = currentText.text;
   textElement.style.color = currentText.color;
+  document.title = currentText.text;
 
   console.log(`Text: ${currentText.text}`);
 
